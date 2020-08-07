53f059389b658d6fbba0ef2cb52219c3df58fd5e
undefined <masnn0@outlook.com>
2020-08-07 20:04:53 +0800

    批量导入用户功能

---
+++ pages/manage_user_import.page.js
@@ -0,0 +1,30 @@
+import { NamedPage } from 'vj/misc/PageLoader';
+import Notification from 'vj/components/notification';
+import i18n from 'vj/utils/i18n';
+import request from 'vj/utils/request';
+import delay from 'vj/utils/delay';
+
+const page = new NamedPage('manage_user_import', () => {
+  async function post(isSubmit) {
+    try {
+      const res = await request.post('', {
+        users: $('[name="users"]').val(),
+        confirm: isSubmit ? 'on' : '',
+      });
+      if (isSubmit) {
+        Notification.success(i18n('Created {0} users.', res.users.length));
+        await delay(2000);
+        window.location.reload();
+      } else {
+        $('[name="messages"]').text(res.messages.join('\n'));
+      }
+    } catch (error) {
+      Notification.error(error.message);
+    }
+  }
+
+  $('[name="preview"]').click(() => post(false));
+  $('[name="submit"]').click(() => post(true));
+});
+
+export default page;
