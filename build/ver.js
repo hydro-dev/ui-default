2fe851cc3254e8f492d0d296ab0d73b3487ef672
undefined <masnn0@outlook.com>
2021-03-25 23:50:22 +0800

    fix version bump

--- build/ver.js
+++ build/ver.js
@@ -2,7 +2,7 @@ const { writeFileSync } = require('fs');
 const { argv } = require('yargs');
 const pkg = require('../package.json');
 
-if (argv.dev) pkg.version += '-dev';
+if (argv.dev) pkg.version = `${pkg.version}-dev`;
 else pkg.version = pkg.version.replace('-dev', '');
 
-writeFileSync('../package.json', JSON.stringify(pkg, null, 2));
+writeFileSync(`${process.cwd()}/package.json`, JSON.stringify(pkg, null, 2));
